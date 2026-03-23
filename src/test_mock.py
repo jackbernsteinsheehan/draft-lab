77cd97358cd73c4314c412776aebeb0bc99c8809
jackbernsteinsheehan <jackbern@uoregon.edu>
2026-03-23 08:56:58 -0700

    updated tests

--- src/test_mock.py
+++ src/test_mock.py
@@ -7,11 +7,13 @@ class TestDraft(unittest.TestCase):
         players = ["Puka Nacua", "Brett Favre", "Jimmy"]
         draft.populate_players(players)
         draft.create_team("Jack")
-        draft.pick_player("Puka Nacua", "Jack")
+        draft.process_pick("Puka Nacua", "Jack")
 
         self.assertEqual(draft.available_players, ["Brett Favre", "Jimmy"])
         self.assertEqual(draft.current_teams, {"Jack": ["Puka Nacua"]})
+    def test_get_players(self):
+        draft = DraftBoard(5, 5)
+        print (type(draft.get_players()[2300]))
 
-    
 if __name__ == "__main__":
     unittest.main()
