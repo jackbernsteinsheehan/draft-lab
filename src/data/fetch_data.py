9b5c1241b7737ea20b8fc0f719e8e936e5663359
jackbernsteinsheehan <jackbern@uoregon.edu>
2026-03-26 19:44:12 -0700

    implemented player fetching

---
+++ src/data/fetch_data.py
@@ -0,0 +1,17 @@
+import nflreadpy as nfl
+import mysql
+import pandas as pd
+
+def get_player_names(year: int):
+    """Pull all player names from nflreadpy"""
+
+    rosters = nfl.load_rosters(year).to_pandas()
+    players = rosters["full_name"]
+    result = []
+    for player in players:
+        result.append(player)
+    return result
+
+    
+if __name__ == "__main__":
+    print(get_player_names(2026))
---
+++ src/data/test_api.py
@@ -0,0 +1,11 @@
+from api import *
+import unittest
+
+class TestDraft(unittest.TestCase):
+
+    def test_get_players(self):
+        
+        print (type(draft.get_players()[2300]))
+
+if __name__ == "__main__":
+    unittest.main()
